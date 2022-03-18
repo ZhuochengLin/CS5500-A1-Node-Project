61b2e8d1723c897426956662d82de193977ca2bd
Zhuocheng Lin <zhuochenglin@zhuochengs-mbp.myfiosgateway.com>
2022-03-17 22:12:04 -0400

    a4 commit

--- models/User.ts
+++ models/User.ts
@@ -2,12 +2,21 @@
  * @file Defines User interface that reflects the fields a user should have.
  */
 import {AccountType} from "./enums/AccountType";
-import {Profile} from "./Profile";
+import {MaritalStatus} from "./enums/MaritalStatus";
+import {Location} from "./Location";
 
 export interface User {
     username: string;
     password: string;
     accountType: AccountType;
     joined: Date;
-    profile: Profile;
+    firstName: string;
+    lastName: string;
+    email: string;
+    profilePhoto: string | null;
+    headImage: string | null;
+    maritalStatus: MaritalStatus;
+    biography: string | null;
+    dateOfBirth: Date | null;
+    location: Location | null;
 }
--- mongoose/UserSchema.ts
+++ mongoose/UserSchema.ts
@@ -4,7 +4,7 @@
 import mongoose from "mongoose";
 import {User} from "../models/User";
 import {AccountType} from "../models/enums/AccountType";
-import ProfileSchema from "./ProfileSchema";
+import {MaritalStatus} from "../models/enums/MaritalStatus";
 
 const UserSchema = new mongoose.Schema<User>({
     username: {type: String, index: true, unique: true, required: true},
@@ -15,7 +15,19 @@ const UserSchema = new mongoose.Schema<User>({
         enum: [AccountType.Personal, AccountType.Academic, AccountType.Professional]
     },
     joined: {type: Date, default: Date.now},
-    profile: {type: ProfileSchema, default: () => ({})}
+    firstName: {type: String, default: ""},
+    lastName: {type: String, default: ""},
+    email: {type: String, default: ""},
+    profilePhoto: {type: String, default: null},
+    headImage: {type: String, default: null},
+    maritalStatus: {
+        type: String,
+        default: MaritalStatus.Single,
+        enum: [MaritalStatus.Single, MaritalStatus.Married, MaritalStatus.Widowed]
+    },
+    biography: {type: String, default: null},
+    dateOfBirth: {type: Date, default: null},
+    location: {type: {longitude: Number, latitude: Number}, default: {longitude: 0.0, latitude: 0.0}}
 }, {collection: "users"});
 
 export default UserSchema;
